d7e232d49d4750c22a69ba8fd1ed7b37e5ef27c2
Oleksandr Drobinin <amperescom@gmail.com>
2024-01-22 08:58:46 +0100

    Simple flow view

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -1,50 +1,59 @@
 'use client';
 
 import * as React from 'react';
-import Xarrow, {useXarrow, Xwrapper} from "react-xarrows";
-import Draggable from 'react-draggable';
+import {Background, ReactFlow, useEdgesState, useNodesState} from "reactflow";
+import 'reactflow/dist/style.css';
+
 
 type Props = {};
 
-const boxStyle = {border: 'grey solid 2px', borderRadius: '10px', padding: '5px'};
-
-
-const DraggableBox = ({id}: { id: string }) => {
-  const updateXarrow = useXarrow();
-  return (
-    <Draggable onDrag={updateXarrow} onStop={updateXarrow}>
-      <div id={id} style={boxStyle}>
-        {id}
-      </div>
-    </Draggable>
-  );
-};
-
-const Example = () => {
-  return (
-    <div
-      style={{display: "flex", justifyContent: "space-evenly", width: "100%"}}
-    >
-      <Xwrapper>
-        <DraggableBox id={'elem1'}/>
-        <DraggableBox id={'elem2'}/>
-        <Xarrow start={'elem1'} end="elem2"/>
-      </Xwrapper>
-
-    </div>
-  );
-
-}
-
 const Page = (props: Props) => {
-  return (
-    <div>
-      <div>
-        <h1>Hello</h1>
-      </div>
-      <Example/>
+  const onInit = (reactFlowInstance: any) => console.log('flow loaded:', reactFlowInstance);
 
-    </div>
+  const initialNodes = [
+    {
+      id: 'hidden-1',
+      type: 'input',
+      data: {label: 'Node 1'},
+      position: {x: 250, y: 5},
+    },
+    {id: 'hidden-2', data: {label: 'Node 2'}, position: {x: 100, y: 100}},
+    {id: 'hidden-3', data: {label: 'Node 3'}, position: {x: 400, y: 100}},
+    {id: 'hidden-4', data: {label: 'Node 4'}, position: {x: 400, y: 200}},
+  ];
+
+  const initialEdges = [
+    {id: 'hidden-e1-2', source: 'hidden-1', target: 'hidden-2'},
+    {id: 'hidden-e1-3', source: 'hidden-1', target: 'hidden-3'},
+    {id: 'hidden-e3-4', source: 'hidden-3', target: 'hidden-4'},
+  ];
+
+  // const initialEdges = [{id: 'e1-2', source: '1', target: '2'}];
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  // const edgesWithUpdatedTypes = edges.map((edge) => {
+  //   if (edge.sourceHandle) {
+  //     // @ts-ignore
+  //     const edgeType = nodes.find((node) => node.type === 'custom').data.selects[edge.sourceHandle];
+  //     edge.type = edgeType;
+  //   }
+
+  //   return edge;
+  // });
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onInit={onInit}
+      attributionPosition="top-right"
+    >
+      <Background color="#aaa" gap={32}/>
+    </ReactFlow>
   );
 };
 
